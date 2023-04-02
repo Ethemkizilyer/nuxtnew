5efe67a2246ae8989fb14e93f8140ca5a96ae997
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-04-02 23:13:52 +0300

    feat: pinia updated

--- stores/cart.js
+++ stores/cart.js
@@ -1,47 +1,60 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-import products from "~~/data";
+// import products from "~~/data";
 
 export const useCartStore = defineStore("cart", {
   state: () => ({
     cartContent: {},
     theme: "light",
+    products: [],
   }),
   getters: {
-    formattedCart(){
-return Object.keys(this.cartContent).map(productId=>{
-    const product=this.cartContent[productId]
-    return {
-      id: product.productId,
-      image: products.find((p) => p.id === product.productId).image,
-      name: products.find((p) => p.id === product.productId).name,
-      price: products.find((p) => p.id === product.productId).price,
-      quantity: product.quantity,
-      cost:
-        product.quantity *
-        products.find((p) => p.id === product.productId).price,
-    };
-})
+    formattedCart() {
+      return Object.keys(this.cartContent).map((productId) => {
+        const product = this.cartContent[productId];
+        return {
+          uuid: product.productId,
+          displayIcon: this.products.find((p) => p.uuid === product.productId)
+            .displayIcon,
+          displayName: this.products.find((p) => p.uuid === product.productId)
+            .displayName,
+          description: this.products.find((p) => p.uuid === product.productId)
+            .description,
+          // price: products.find((p) => p.uuid === product.productId).price,
+          quantity: product.quantity,
+          // cost:
+          //   product.quantity *
+          //   products.find((p) => p.id === product.productId).price,
+        };
+      });
     },
-    total(){
-        return Object.keys(this.cartContent).reduce((acc,id)=>{
-            const product =products.find(p=> p.id === id)
-            if(product){
-                return acc + product.price * this.cartContent[id].quantity
-            }
-            return acc + 0
-        },0)
+    total() {
+      return Object.keys(this.cartContent).reduce((acc, id) => {
+        const product = this.products.find((p) => p.uuid === id);
+        if (product) {
+          return acc + product.price * this.cartContent[id].quantity;
+        }
+        return acc + 0;
+      }, 0);
     },
-    productsTotal(){
-        return Object.keys(this.cartContent).reduce((acc,id)=>{
-            return acc + this.cartContent[id].quantity
-        },0)
+    productsTotal() {
+      return Object.keys(this.cartContent).reduce((acc, id) => {
+        return acc + this.cartContent[id].quantity;
+      }, 0);
     },
-    getTheme(){
-        return this.theme;
+    getTheme() {
+      return this.theme;
     },
   },
   actions: {
+    async fetchProducts() {
+      try {
+        const response = await axios.get("https://valorant-api.com/v1/agents");
+        this.products = response.data.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
     add(productId) {
       if (this.cartContent.hasOwnProperty(productId)) {
         this.cartContent[productId] = {
@@ -55,22 +68,22 @@ return Object.keys(this.cartContent).map(productId=>{
         };
       }
     },
-    remove(productId){
-        if(!this.cartContent[productId]){
-            return
-        }
-        this.cartContent[productId].quantity -= 1;
+    remove(productId) {
+      if (!this.cartContent[productId]) {
+        return;
+      }
+      this.cartContent[productId].quantity -= 1;
 
-        if(this.cartContent[productId].quantity === 0){
-            delete this.cartContent[productId]
-        }
+      if (this.cartContent[productId].quantity === 0) {
+        delete this.cartContent[productId];
+      }
     },
-    removeProduct(productId){
-        delete this.cartContent[productId]
+    removeProduct(productId) {
+      delete this.cartContent[productId];
     },
-    toogleTheme(){
-      console.log(this.theme)
-        this.theme = (this.theme === "light" ? "dark" : "light")
-    }
+    toogleTheme() {
+      console.log(this.theme);
+      this.theme = this.theme === "light" ? "dark" : "light";
+    },
   },
 });
