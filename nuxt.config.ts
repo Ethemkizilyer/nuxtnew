d0c6c9893e54c8536663281311d557103e05c983
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-04-03 03:38:28 +0300

    feat: router and font added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 
 export default defineNuxtConfig({
+
   css: [
     "vuetify/styles",
     "@mdi/font/css/materialdesignicons.min.css",
     "@fortawesome/fontawesome-free/css/all.css",
+    "@/static/fonts/ValorantFont.ttf",
   ],
   build: {
     transpile: ["vuetify"],
   },
-  modules:[
-    '@pinia/nuxt'
-  ]
+  modules: ["@pinia/nuxt"],
 });
